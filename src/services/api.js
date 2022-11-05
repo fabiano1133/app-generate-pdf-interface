f3c7e388e5935276c370d0d18900a605667e3ded
fabiano1133 <fabiano.santos0@icloud.com>
2022-11-05 20:29:24 -0300

    refactor: update api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://deply.fabiano-development.tech/',
-    headers: { 
-        'Content-Type': 'application/pdf', 
-        'Accept': 'application/pdf', 
-        'Access-Control-Allow-Origin': '*', 
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS', 
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-        }
-        
+    baseURL: 'https://deply.fabiano-development.tech/'
 })
