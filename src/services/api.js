7994c268aa1a6cbd94d38290f8070b4c44948f45
fabiano1133 <fabiano.santos0@icloud.com>
2022-11-05 19:36:11 -0300

    refactor: api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://deply.fabiano-development.tech',
+    baseURL: 'https://deply.fabiano-development.tech/',
     headers: { 
         'Content-Type': 'application/pdf', 
         'Accept': 'application/pdf', 
