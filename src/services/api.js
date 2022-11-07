11aa542d1b5f2c4b9b7688d27bf9aec502939889
fabiano1133 <fabiano.santos0@icloud.com>
2022-11-07 10:35:08 -0300

    feat: spinner loading

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    //baseURL: 'https://deply.fabiano-development.tech/'
-    baseURL: 'http://localhost:3333/'
+    baseURL: 'https://deply.fabiano-development.tech/'
 })
