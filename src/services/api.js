b813ec36104963affe31e99a35c37846f09a59fa
fabiano1133 <fabiano.santos0@icloud.com>
2022-11-16 14:47:36 -0300

    refactor: alterando a baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
-  // baseURL: 'https://deply.fabiano-development.tech/'
+  //baseURL: "http://localhost:3333",
+  baseURL: "https://deply.fabiano-development.tech/",
 });
