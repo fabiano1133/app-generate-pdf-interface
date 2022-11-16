f0a44b8c5f906a2b1229ee5ae650ec1e90edef9c
fabiano1133 <fabiano.santos0@icloud.com>
2022-11-16 13:58:46 -0300

    feat: rename file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./components/Home/Home";
+import { Home } from "./components/Home/Hm";
 import FormCaucao from "../src/components/Form-Caucao/Form-caucao.jsx";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
---
+++ src/components/Home/Hm.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+import { NavLink } from "react-router-dom";
+export function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="container">
+          <h1 className="title">Seja bem vindo!</h1>
+          <h2 className="subTitle">Gere contratos de forma simples</h2>
+        </div>
+
+        <div className="card-container">
+          <div className="card-options">
+            <h2>Qual o tipo de contrato que você precisa?</h2>
+            <div className="options">
+              <NavLink to="/form-fiador" className="btn-option">
+                ALUGUEL COM FIADOR
+              </NavLink>
+              <NavLink to="/form-caucao" className="btn-option">
+                ALUGUEL COM CAUÇÃO
+              </NavLink>
+              <NavLink className="btn-option">COMPRA E VENDA</NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
